bb0ebb69f01c0b6eb1935629458548c25c22f885
Samyak Bhuta <samyak.bhuta@gmail.com>
2014-06-17 04:25:13 +0530

    Adds subset for Vowels and Consonants
    - Subset for 'IndependentVowel'
    - Subset for 'DependentVowel'
    - Subset for 'Vowel' (incluses both the above)
    - Subset for 'Consonant'

--- _gu.js
+++ _gu.js
@@ -4,8 +4,39 @@ var _uni = require('unilities');
 // 128, length of Gujarati Unicode Block
 var _gu = new _uni ( 2688, 128, 'Gujarati' );
 
+var independentVowels = _gu.charcodes(2693,2701) // [અ] to [ઍ]
+			.concat( _gu.charcodes(2703,2705) )  // [એ] to [ઑ]
+			.concat( _gu.charcodes(2707,2708) )  // [ઓ] and [ઔ]
+			.concat( _gu.charcodes(2784,2785) ); // [ૠ] and [ૡ]
+
+var dependentVowels = _gu.charcodes(2750,2757)
+			.concat( _gu.charcodes(2759,2761) )
+			.concat( _gu.charcodes(2763,2764) )
+			.concat( _gu.charcodes(2786,2787) );
+
+var vowels = independentVowels.concat(dependentVowels);
+
+var consonants = _gu.charcodes(2709,2728)	// [ક] to [ન]
+		.concat( _gu.charcodes(2730,2736) ) // [પ] to [ર]
+		.concat( _gu.charcodes(2738,2739) ) // [લ] and [ળ]
+		.concat( _gu.charcodes(2741,2745) ) // [વ] to [હ]
+
+var digits = _gu.charcodes(2790,2799); // [૦] to [૯]
+
+// Adds Gujarati IndependentVowels as subset
+_gu.addSubset( 'IndependentVowel', independentVowels );
+
+// Adds Gujarati DependentVowels as subset
+_gu.addSubset( 'DependentVowel', dependentVowels );
+
+// Adds Gujarati Vowels as subset
+_gu.addSubset( 'Vowel', vowels );
+
+// Adds Gujarati Consonants as subset
+_gu.addSubset( 'Consonant', consonants );
+
 // Adds Gujarati Digits as subset
-_gu.addSubset('Digit', _gu.charcodes(2790,2799));
+_gu.addSubset( 'Digit', digits );
 
 // Returns the updated module
 module.exports = _gu;
